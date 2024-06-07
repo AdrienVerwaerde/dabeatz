b385694213416237e38f0973451dec7d5a4d0865
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2024-06-08 00:45:05 +0200

    better info block

--- src/components/InfoBlock.tsx
+++ src/components/InfoBlock.tsx
@@ -1,8 +1,8 @@
 
 export default function InfoBlock() {
     return (
-        <div>
-            <h1 className="font-mono text-sm md:text-xl text-center pt-4 px-4 text-muted-foreground">Buy copyright-free samples for personal or professional use !</h1>
+        <div className="flex justify-center items-center pt-4">
+            <h1 className="font-mono text-sm md:text-xl text-center px-4 text-muted-foreground bg-gray-100 w-full">🎧 Royalty-free samples to use in any way you like 🎧</h1>
         </div>
     )
 }
