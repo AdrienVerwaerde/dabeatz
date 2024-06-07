45ac36201a1ef38f3841e2e2fa081265472a2434
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2024-06-08 00:50:26 +0200

    links to new tab

--- src/app/(customerFacing)/about/page.tsx
+++ src/app/(customerFacing)/about/page.tsx
@@ -6,7 +6,7 @@ export default function About() {
         <div className="flex flex-col justify-center items-center container text-center font-mono">
             <h1>This website was made by Adrien Verwaerde</h1>
             <p className="text-base text-muted-foreground text-center">Using Next.js, Tailwind, Stripe and Prisma</p>
-            <a href="https://adrienverwaerde.github.io/Portfolio/"><Button className="font-sans text-2xl bg-pink-800 hover:bg-black p-6 mt-4">Portfolio</Button></a>
+            <a href="https://adrienverwaerde.github.io/Portfolio/" target="_blank"><Button className="font-sans text-2xl bg-pink-800 hover:bg-black p-6 mt-4">Portfolio</Button></a>
         </div>
     )
 }
