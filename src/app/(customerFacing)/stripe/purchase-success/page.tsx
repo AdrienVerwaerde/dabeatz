a9e5c1513b2272123f1ac54fffe65917c7fb34a1
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2024-06-08 15:14:23 +0200

    edit payment success page

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -27,7 +27,7 @@ export default async function SuccessPage({
 
   return (
     <div className="max-w-5xl w-full mx-auto space-y-8">
-      <h1 className="text-4xl font-bold">
+      <h1 className="text-4xl font-bold font-mono">
         {isSuccess ? "Success!" : "Error!"}
       </h1>
       <div className="flex gap-4 items-center">
@@ -40,11 +40,11 @@ export default async function SuccessPage({
           />
         </div>
         <div>
-          <div className="text-lg">
+          <div className="text-lg font-sans">
             {formatCurrency(product.priceInCents / 100)}
           </div>
-          <h1 className="text-2xl font-bold">{product.name}</h1>
-          <div className="line-clamp-3 text-muted-foreground">
+          <h1 className="text-2xl font-bold font-sans uppercase">{product.name}</h1>
+          <div className="line-clamp-3 text-muted-foreground font-mono">
             {product.description}
           </div>
           <div className="flex flex-col">
