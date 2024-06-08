6d9fee73eef266115542d3a65e3f887e39e388de
AdrienVerwaerde <adrien.verwaerde@oclock.school>
2024-06-08 15:36:55 +0200

    few more changes

--- src/components/InfoBlock.tsx
+++ src/components/InfoBlock.tsx
@@ -2,8 +2,8 @@
 export default function InfoBlock() {
     return (
         <div className="flex justify-center items-center mt-4 bg-gray-100">
-            <span className="pl-4 sm:px-2">🎧</span>
-            <h1 className="font-mono text-sm md:text-xl text-center px-4 text-muted-foreground">Royalty-free samples to use in any way you like</h1>
+            <span className="pl-4">🎧</span>
+            <h1 className="font-mono text-sm md:text-xl text-center px-2 md:px-4 text-muted-foreground">Royalty-free samples to use in any way you like</h1>
             <span className="pr-4">🎧</span>
         </div>
     )
